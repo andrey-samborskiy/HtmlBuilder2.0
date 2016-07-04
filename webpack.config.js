75e088b3f861189dbba2988fd8af29ae4a56e3dc
Andrey Samborskiy <andyhell@mail.ru>
2016-07-04 12:35:45 +0300

    add postcss, autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-var path = require('path');
+var path			  = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var precss       	  = require('precss');
+var autoprefixer 	  = require('autoprefixer');
 
 module.exports = {
 	entry: path.resolve('src/app'),
@@ -16,15 +18,22 @@ module.exports = {
 			},
 			{
 				test: /\.scss$/,
-				loader: ExtractTextPlugin.extract('style','css!sass') },
+				loader: ExtractTextPlugin.extract('style','css!postcss?pack=cleaner!sass') },
 			{
 				test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
 				include: [path.resolve(__dirname, 'src/static')],
 				loader: 'file?name=[1]&regExp=src\\\\static\\\\(.*)' },  // "\\\\" -hack for windows path :(
 			{
 				test: /\.jade$/,
-				loader: 'jade?pretty=true' }
+				loader: 'jade?pretty=true'
+			}
 		]
+	},
+	postcss: function () {
+		return {
+			default: [precss, autoprefixer],
+			cleaner: [autoprefixer({browsers:["last 4 version"]})]
+		}
 	},
 	plugins: [
 		new HtmlWebpackPlugin({  // for other files add new plugin
